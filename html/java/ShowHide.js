fa6b799bb0a1b7ab7cb994e20ca22ab9572a79ee
Juan Casimiro <juan.a.casimiro@outlook.com>
2019-05-20 14:51:05 -0700

    Add files via upload

---
+++ html/java/ShowHide.js
@@ -0,0 +1,8 @@
+function ShowHide() {
+  var x = document.getElementById("space");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} 
