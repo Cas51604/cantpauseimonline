2e33efaeae3ad3f7c6c54906c8dddf5feb93c317
Cas51604 <juan.a.casimiro@outlook.com>
2020-04-21 10:27:53 -0700

    Update sfooter.php

--- html/sfooter.php
+++ html/sfooter.php
@@ -1,5 +1,5 @@
 <footer class="w3-container w3-padding-16">
-    <h5>Capstone Project for CSU CI Channel Islands</h5>
+    <h5></h5>
     <br>
-    <h10>© 2018 cantpauseimonline.com created by Juan Casimiro</h10>
+    <h10>© 2018 cantpauseimonline.com</h10>
 </footer>
